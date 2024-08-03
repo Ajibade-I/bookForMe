62bd882f8e133e58299616b128dfad0702751a97
Ajibade <isiakaajibade581@gmail.com>
2024-08-03 21:40:37 +0100

    Change welcome

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,14 +2,14 @@ const express = require("express");
 const {
   addAdmin,
   adminLogin,
-  hello,
+
   getAllGuests,
 } = require("../controllers/adminController");
 const router = express.Router();
 
 router.post("/registration", addAdmin);
 router.post("/login", adminLogin);
-router.get("/", hello);
+
 router.get("/get/guests", getAllGuests);
 
 module.exports = router;
